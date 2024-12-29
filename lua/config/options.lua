6aee5565f30d45f796b76f8720d889d979e74cbb
Dev Vrat Singh <itsdvvs.18@gmail.com>
2024-12-29 18:02:55 +0530

    setup package manager and basic options

---
+++ lua/config/options.lua
@@ -0,0 +1,10 @@
+local option = vim.opt
+
+vim.g.mapleader        = " "
+vim.g.maplocalleader   = "\\"
+
+option.number          = true
+option.relativenumber  = true
+option.tabstop         = 2
+option.shiftwidth      = 2
+option.hlsearch        = false
