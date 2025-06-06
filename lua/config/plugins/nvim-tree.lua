939901b447487b7216c2779f52753665c4fd71f3
Dev Vrat Singh <itsdvvs.18@gmail.com>
2025-06-06 09:32:22 +0530

    changed the toggle key for tree-explorer

--- lua/config/plugins/nvim-tree.lua
+++ lua/config/plugins/nvim-tree.lua
@@ -7,6 +7,7 @@ return {
   },
   config = function()
     require("nvim-tree").setup {}
-		vim.keymap.set("n", "<leader>e", "<CMD>NvimTreeToggle<CR>", { desc = "Open file explorer" })
+		vim.keymap.set("n", "<leader>ee", "<CMD>NvimTreeToggle<CR>", { desc = "File explorer (current dir)" })
+		vim.keymap.set("n", "<leader>ec", "<CMD>NvimTreeToggle<CR>", { desc = "File explorer (Neovim dir)" })
   end,
 }
